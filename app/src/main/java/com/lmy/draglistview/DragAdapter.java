ef8d3c777662041d87748e90683a78fe9d4defa8
liumingyu <liumingyu@vargo.com.cn>
2018-11-23 17:19:22 +0800

    first commit

---
+++ app/src/main/java/com/lmy/draglistview/DragAdapter.java
@@ -0,0 +1,224 @@
+package com.lmy.draglistview;
+
+import android.content.Context;
+import android.graphics.Rect;
+import android.os.Build;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by langge on 18-3-27.
+ */
+public class DragAdapter extends BaseAdapter {
+
+    private Context mContext;
+    private int finalAdded = 0;
+    private List<DragItemEntiy> mAddedItems;
+    private List<DragItemEntiy> mNotAddedItems;
+
+    public static final int TYPE_TAG = 0;
+    public static final int TYPE_ITEM = 1;
+
+
+    private String tag = "";
+
+    public DragAdapter(Context context, ArrayList<DragItemEntiy> added, ArrayList<DragItemEntiy> notAdded, String centerTip) {
+        this.mContext = context;
+        mAddedItems = added;
+        mNotAddedItems = notAdded;
+        finalAdded = mAddedItems.size();
+
+        tag = centerTip;
+    }
+
+    @Override
+    public int getCount() {
+        return 1+mAddedItems.size()+mNotAddedItems.size();
+    }
+
+
+    @Override
+    public int getViewTypeCount() {
+        return 2;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if(position==mAddedItems.size()){
+            return TYPE_TAG;
+        }else {
+            return TYPE_ITEM;
+        }
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        ViewHolder viewHolder=null;
+        if (convertView==null){
+            if (getItemViewType(position)==TYPE_ITEM){
+                convertView = LayoutInflater.from(mContext).inflate(R.layout.drag_list_item, null);
+                viewHolder = new ViewHolder(convertView);
+            }else {
+                convertView = LayoutInflater.from(mContext).inflate(R.layout.drag_list_item_tag, null);
+                viewHolder = new ViewHolder(convertView);
+            }
+        }else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+
+        DragItemEntiy itemObj = getLDragItem(position);
+        if(getItemViewType(position)==TYPE_ITEM){
+            viewHolder.nameView.setText(itemObj.name);
+        }else {
+            viewHolder.nameView.setText(tag);
+            viewHolder.nameView.setEnabled(false);
+        }
+
+        return convertView;
+    }
+
+
+    class ViewHolder{
+        TextView nameView;
+        ImageView iconView;
+        public ViewHolder(View view){
+            nameView = (TextView)view.findViewById(R.id.drag_list_item_text);
+
+            view.setTag(this);
+        }
+    }
+
+
+    private DragItemEntiy getLDragItem(int position){
+        if (position==mAddedItems.size()){
+            return null;
+        }
+        else if(position<mAddedItems.size()){
+            //add
+            return mAddedItems.get(position);
+        }else{
+            //not add
+            return mNotAddedItems.get(position-mAddedItems.size()-1);
+        }
+    }
+
+
+    public boolean isTag(int pos){
+        if( getItemViewType(pos)==TYPE_TAG){
+            return true;
+        }
+        return false;
+    }
+
+    public boolean canDrag(int pos, View dragView, int x, int y){
+
+
+        if( getItemViewType(pos)==TYPE_TAG){
+            return false;
+        }
+        else {
+            if (dragView == null) {
+                return false;
+            }
+            View dragger = dragView.findViewById(R.id.drag_list_item_drag);
+            if (dragger == null || dragger.getVisibility() != View.VISIBLE) {
+                return false;
+            }
+
+            float tx = x - getViewX(dragView);
+            float ty = y - getViewY(dragView);
+            Rect mFrame = new Rect();
+            dragger.getHitRect(mFrame);
+            if (mFrame.contains((int) tx, (int) ty)) { // 当点击拖拽图标才可进行拖拽
+                return true;
+            }
+
+            return false;
+        }
+    }
+
+
+    public boolean exchange(int scrPos, int desPos){
+        boolean success = false;
+        int count = mAddedItems.size();
+        if(scrPos==count || desPos==count){
+            return false;
+        }
+
+        if(scrPos!=desPos){
+            if(scrPos<count){
+                DragItemEntiy srcObj = mAddedItems.get(scrPos);
+                if (desPos<count){
+                    //change in add rang
+                    DragItemEntiy desObj = mAddedItems.get(desPos);
+                    mAddedItems.set(scrPos, desObj);
+                    mAddedItems.set(desPos, srcObj);
+                }else {
+                    //change from add to notAdd
+                    desPos = desPos-mAddedItems.size()-1;
+                    DragItemEntiy desObj = mNotAddedItems.get(desPos);
+                    mAddedItems.set(scrPos, desObj);
+                    mNotAddedItems.set(desPos, srcObj);
+                }
+
+            }else {
+                scrPos = scrPos-mAddedItems.size()-1;
+                DragItemEntiy srcObj = mNotAddedItems.get(scrPos);
+                if (desPos<count){
+                    //change from noTAdd to add
+                    DragItemEntiy desObj = mAddedItems.get(desPos);
+                    mAddedItems.set(desPos, srcObj);
+                    mNotAddedItems.set(scrPos, desObj);
+                }else {
+                    //change in notAdd rang
+                    desPos = desPos-mAddedItems.size()-1;
+                    DragItemEntiy desObj = mNotAddedItems.get(desPos);
+                    mNotAddedItems.set(scrPos, desObj);
+                    mNotAddedItems.set(desPos, srcObj);
+                }
+            }
+            success = true;
+        }
+
+        if (success) {
+            notifyDataSetChanged();
+        }
+        return success;
+
+    }
+
+    public float getViewX(View view) {
+        if (Build.VERSION.SDK_INT >= 11) {
+            return view.getX();
+        } else {
+            return view.getLeft() + view.getTranslationX();
+        }
+    }
+
+    public float getViewY(View view) {
+        if (Build.VERSION.SDK_INT >= 11) {
+            return view.getY();
+        } else {
+            return view.getTop() + view.getTranslationY();
+        }
+    }
+
+}
---
+++ app/src/main/java/com/lmy/draglistview/DragItemEntiy.java
@@ -0,0 +1,9 @@
+package com.lmy.draglistview;
+
+/**
+ * Created by langge on 18-3-29.
+ */
+public class DragItemEntiy {
+    String name;
+
+}
